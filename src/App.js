0a6226e0767df9a0fd5b91f019a1d6f9cb05ddc8
Daniel Egidio <danielegidioti@gmail.com>
2021-06-08 14:24:43 -0300

    estruturação do todolist

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="todo-app">
+        <TodoList/>
+
+    </div>
+  );
+}
+
+export default App;
