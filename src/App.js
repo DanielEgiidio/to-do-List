d73c78e18286de1231e6306e1df67be882885a47
Daniel Egidio <danielegidioti@gmail.com>
2021-06-08 15:02:38 -0300

    Implementação do CSS

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import react from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
+import './App.css';
+
 function App() {
   return (
     <div className="todo-app">
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 
 export default function TodoForm(props) {
-    const [ input, setInput ] = useState('');
+    const [ input, setInput ] = useState(props.edit ? props.edit.value: '');
 
     const inputRef = useRef(null);
 
@@ -25,17 +25,36 @@ export default function TodoForm(props) {
     };
 
     return (
-        <form className='todo-form' onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder='Adicionar To-do'
+        <form onSubmit={handleSubmit} className='todo-form'>
+          {props.edit ? (
+            <>
+              <input
+                placeholder='Update your item'
                 value={input}
+                onChange={handleChange}
+                name='text'
+                ref={inputRef}
+                className='todo-input edit'
+              />
+              <button onClick={handleSubmit} className='todo-button edit'>
+                Update
+              </button>
+            </>
+          ) : (
+            <>
+              <input
+                placeholder='Add a todo'
+                value={input}
+                onChange={handleChange}
                 name='text'
                 className='todo-input'
-                onChange={handleChange}
                 ref={inputRef}
-            />
-            <button className='todo-button'>Add</button>
+              />
+              <button onClick={handleSubmit} className='todo-button'>
+                Add todo
+              </button>
+            </>
+          )}
         </form>
-    )
-}
+      );
+    }
